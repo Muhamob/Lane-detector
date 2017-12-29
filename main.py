67e63d8a2a3f7280ee97ae64f87d2f91a5692349
Muhamob <muhamob@gmail.com>
2017-12-29 18:26:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import numpy as np
 import roi
 import drawing as draw
 import extrapolation as ex
+
 filename = 'video/highway1.mp4'
 cap = cv.VideoCapture(filename)
+
+framecnt = 0
+llinecnt = 0
+rlinecnt = 0
 
 while True:
     _, frame = cap.read()
@@ -12,7 +17,7 @@ while True:
     _, thr = cv.threshold(gray, 100, 255, cv.THRESH_BINARY)
     edges = cv.Canny(thr, 0, 166)
 
-    points = np.array([[[72, 268], [269, 179], [352, 187],
+    points = np.array([[[72, 268], [269, 190], [352, 190],
                         [462, 262]]], dtype=np.int32)
     mask = roi.mask(points, frame=edges)
     masked_image = roi.applyMask(edges, mask)
@@ -20,14 +25,21 @@ while True:
     #                      max_theta=np.pi/2, min_theta=-np.pi/2)
     lines = cv.HoughLinesP(masked_image, rho=2, theta=np.pi/180, threshold=20,
                     minLineLength=20, maxLineGap=100)
-    
-    # draw.drawlinesP(frame, lines, (0, 240, 0), 2)
-    ex.draw_lane_lines(frame, ex.lane_lines(frame, lines))
+    left_lines, right_lines = ex.separateLines(lines)
+    left_mean = ex.averageLines(left_lines)
+    right_mean = ex.averageLines(right_lines)
+    framecnt += 1
+    llinecnt += len(left_mean)
+    rlinecnt += len(right_mean)
+    leftLine = ex.lstsqLine(left_lines)
+    rightLine = ex.lstsqLine(right_lines)
+    frame = draw.drawlinesP(frame, leftLine, (0, 51, 255), 3)
+    frame = draw.drawlinesP(frame, rightLine, (102, 153, 51), 3)
     cv.imshow('highway in Moscow', frame)
-    # cv.imshow('edges', thr)
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break
 
+print(rlinecnt, llinecnt, framecnt)
 cap.release()
 cv.destroyAllWindows()
